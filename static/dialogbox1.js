1d70dffe00382a40ad0087e1e5b778d874a7dd6a
lefffan <lephan@mail.ru>
2024-07-24 15:08:33 +0700

    Another vs code test 2

--- static/dialogbox1.js
+++ static/dialogbox1.js
@@ -1,4 +1,3 @@
-//hui
 const DIALOGSELECTABLEELEMENTMAXOPTIONS	= 1024;
 const BUTTONTIMERMAXSECONDS				= 60 * 60 * 24 * 7; // One week
 const ELEMENTUSERPROPS					= ['path', 'type', 'head', 'hint', 'data', 'flag', 'accl'];
